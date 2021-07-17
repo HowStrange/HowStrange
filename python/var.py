0be9b1278e0676634ab22f7fd060e200b5031ce5
Alan Silva <strange@localhost.localdomain>
2021-07-17 08:09:06 -0300

    Declarando e concatenando variaveis

---
+++ python/var.py
@@ -0,0 +1,15 @@
+nome = 'Alan Silva'
+nascimento = 2001
+idade = 2021 - nascimento
+cidade = 'São paulo - SP'
+salario = 1.900
+filhos = False
+
+if(filhos):
+    filhos = 'Tenho filhos.'
+else:
+    filhos = 'Não tenho filhos.'
+
+print("Olá meu nome é ", nome, " Eu tenho ", idade, " Anos de idade, moro em ", cidade, ",",filhos)
+
+print(type(nome))
